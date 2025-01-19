e1ab3b495bbce88cd9e427f5ece235e0eeb22d7f
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2025-01-19 21:35:50 +0300

    123213

--- api/patch.php
+++ api/patch.php
@@ -1,8 +1,8 @@
 <?php
-header('Content-Type: text/plain');
+header('Content-Type: text/plain; charset=utf-8');
 
 echo "version=3\n";
-echo "serverpath=https://raw.githubusercontent.com/ElementoBorzi/test-cdn/main/patches\n";
+echo "serverpath=https://raw.githubusercontent.com/ElementoBorzi/test-cdn/main\n";
 echo "path=\n\n";
 
 echo "file=Data/ruRU/wow-update-ruRU-20001.MPQ\n";
@@ -12,4 +12,3 @@ echo "    fileversion=20001\n";
 echo "    flags=4\n";
 echo "    path=base\n";
 echo "    tag=ruRU\n";
-?>
